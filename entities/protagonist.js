5dcb3875850cf08341206222090008ae0f077baa
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-07-24 15:23:27 -0400

    protagonist (tenet reference)

---
+++ entities/protagonist.js
@@ -0,0 +1,9 @@
+class protagonist extends Entity {
+    constructor(handler, scene, entityName) {
+        super(handler, scene, entityName);
+    }
+
+    update() {
+        super.update();
+    }
+}
