c625476e755efbe137bbfc0622d3f01f0624d4c9
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-07-29 03:48:37 -0400

    Added about

--- scenes/main-menu.js
+++ scenes/main-menu.js
@@ -5,24 +5,41 @@ class MainMenu extends Phaser.Scene {
 
     preload() {
         this.load.image("coverart", "image/title image-4.png.png");
+        this.load.image("credits", "image/about-popup.png");
         this.load.spritesheet("playbutton", "image/2worldbuttons.png", { frameWidth: 320, frameHeight: 640 / 4 });
     }
 
     create() {
         this.coverart = this.add.image(gameWidth / 2, gameHeight / 2, "coverart");
         this.coverart.setScale(gameWidth / 128);
-        
-        this.playbutton = this.add.sprite(gameWidth / 2, gameHeight / 2 + 123, "playbutton", 0);
+        this.credits = this.add.image(gameWidth / 2, gameHeight / 2, "credits");
+        this.credits.setScale(gameWidth / 2000);
+        this.credits.visible = false;
+
+        this.playbutton = this.add.sprite(125, gameHeight - 70, "playbutton", 0);
         this.playbutton.setScale(0.69);
         this.playbutton.setInteractive();
         this.playbutton.on("pointerover", function() {
             this.playbutton.setTexture("playbutton", 1);
-        }.bind(this));  
+        }.bind(this));
         this.playbutton.on("pointerout", function() {
             this.playbutton.setTexture("playbutton", 0);
         }.bind(this));
         this.playbutton.on("pointerdown", function() {
             this.scene.start("level1");
+        }.bind(this));
+
+        this.aboutButton = this.add.sprite(gameWidth - 125, gameHeight - 70, "playbutton", 2);
+        this.aboutButton.setScale(0.69);
+        this.aboutButton.setInteractive();
+        this.aboutButton.on("pointerover", function() {
+            this.aboutButton.setTexture("playbutton", 3);
+        }.bind(this));
+        this.aboutButton.on("pointerout", function() {
+            this.aboutButton.setTexture("playbutton", 2);
+        }.bind(this));
+        this.aboutButton.on("pointerdown", function() {
+            this.credits.visible = !this.credits.visible;
         }.bind(this));
     }
 
